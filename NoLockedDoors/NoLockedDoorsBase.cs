9dc1d08803c723559123ae73a89c9b3e3748cd65
Tamion <70228790+notTamion@users.noreply.github.com>
2024-01-15 17:42:13 +0100

    Initial Commit

---
+++ NoLockedDoors/NoLockedDoorsBase.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using NoLockedDoors.Patches;
+
+namespace NoLockedDoors;
+
+[BepInPlugin(modGUID, modName, modVersion)]
+public class NoLockedDoorsBase : BaseUnityPlugin
+{
+    private const string modGUID = "tamion.NoLockedDoors";
+    private const string modName = "NoLockedDoors";
+    private const string modVersion = "1.0.1";
+    
+    private readonly Harmony harmony = new Harmony(modGUID);
+    internal ManualLogSource logger;
+    
+    private void Awake()
+    {
+        logger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+        
+        logger.LogInfo("NoLockedDoors loaded");
+        
+        harmony.PatchAll(typeof(DoorLockPatch));
+    }
+}
